337b2d7075c1e28f2ff34b4f7cc681c5c8390a80
Maxime LEDEE <ledee.maxime@gmail.com>
2015-12-31 07:10:58 +0100

    Add generateReport() function and Fixations

--- main.sh
+++ main.sh
@@ -30,7 +30,9 @@ ORGANISATION="Master Degree Bioinformatics - Rouen University"
 function init {
 	echo "init function called"
 	DIR="$( cd "$( dirname "${0}" )" && pwd )"
-	mkdir -p ${DIR}"/Data" ${DIR}"/Log" ${DIR}"/Reports"
+	dir_report=${DIR}"/Reports"
+	dir_tmpReport=${dir_report}"/tmp"
+	mkdir -p ${DIR}"/Data" ${DIR}"/Log" ${dir_report}
 	scriptGestionUsers=${DIR}"/Bin/gestion_users.sh"
 	scriptGestionRepatriations=${DIR}"/Bin/gestion_repatriations.sh"
 	scriptGestionStrategies=${DIR}"/Bin/gestion_strategies.sh"
@@ -39,17 +41,13 @@ function init {
 	fileListRepatriations=${DIR}"/Data/list_repatriations.txt"
 	fileLog=${DIR}"/Log/get_data.log"
 	logFileTmp="log_wget.tmp"
-	# TODO :
-	# fileTexModel=
-	# fileHtmlModem=
-	# dirData=
-	# ... ?
+	dir_modelReport="${dir_report}/.model"
 }
 
 # Display the main menu. 4 choices are available.
 function displayMainMenu {
 	echo "displayMainMenu function called"
-	choice=`zenity --list --title="Main menu" --text="Select a choice :" --width=320 --height=210 \
+	choice=`zenity --list --title="Main menu" --text="Select a choice :" --width=320 --height=230 \
 		--column="" --column="Description" \
 		1 "Manage users" \
 		2 "Manage repatriations" \
@@ -89,23 +87,173 @@ function displayMenuGenerationReport {
 
 	listUsers=$(cat ${fileListUsers} | tr "\n" "," | sed "s/,$//")
 
-	generateReport=`yad --width=400 --center --title="Generate Report" --text="Please choose which report you want to generate: You can select one or all users. The start and end dates are optional." \
-	--form --item-separator="," \
+	infosReport=`yad --width=400 --center --separator="#" --title="Generate Report" --text="Please choose which report you want to generate: You can select one or all users. The start and end dates are optional." \
+	--form --item-separator="," --date-format="%d-%m-%Y" \
 	--field="Select user :":CB \
 	--field="Choose a start date (Optional)":DT \
 	--field="Choose an end date (Optional)":DT \
 	--field="Select a format of report :":CB \
-	"All the users,${listUsers}" "" "" "Document,Website,Slides" `
+	"ALL,${listUsers}" "" "" "Document" ` # TODO: "Document,Website,Slides"
 
-	echo $generateReport
+	if [ -z "$infosReport" ]
+	then
+		echo "Canceling"
+	else
+		echo $infosReport
+		local idUser=`echo $infosReport | cut -d# -f1 | cut -d: -f1`
+		local dateStart=`echo $infosReport | cut -d# -f2`
+		if [ -z $dateStart ]; then
+			dateStart="01-01-0001" 
+		fi
+		local dateEnd=`echo $infosReport | cut -d# -f3`
+		if [ -z $dateEnd ]; then
+			dateEnd=`date "+%d-%m-%Y"`
+		fi
+		local typeReport=`echo $infosReport | cut -d# -f 4`
+		generateReport ${idUser} ${dateStart} ${dateEnd} ${typeReport}
+	fi
+	displayMainMenu
 }
 
 # Generate a report based on a user and/or a period.
 # Arguments: a user, a period (the beginning and the end), a type of report (website, document or slide).
-# TODO: generate a model (html or tex).
 # TODO: mettre un lien symbolique cliquable pour la var $destFile
+# Rappel format d'un log :
+# $date;$idUser;$lastNameUser;$firstNameUser;$mailUser;$destRepatriation;$sourceRepatriation;$idStatus:$status:$taille
 function generateReport {
 	echo "generateReport function called"
+	local listIdUser=$1
+	local dateStart=$2
+	local dateEnd=$3
+	local typeReport=$4
+	echo -e "\t listIdUser : $listIdUser"
+	echo -e "\t dateStart : $dateStart"
+	echo -e "\t dateEnd : $dateEnd"
+	echo -e "\t typeReport : $typeReport"
+	
+	########
+	# Init #
+	########
+	
+	mkdir ${dir_tmpReport}
+	cp ${dir_modelReport}/${typeReport}/*.tex ${dir_tmpReport}/
+	tex_modelStart="${dir_tmpReport}/model_start.tex"
+	tex_modelSummaryForALLuser="${dir_tmpReport}/model_summaryForALLuser.tex"
+	tex_modelOneUser="${dir_tmpReport}/model_oneUser.tex"
+	tex_modelEnd="${dir_tmpReport}/model_end.tex"
+	tex_multipleUsers="${dir_tmpReport}/multipleUsers.tex"	
+	userName=`exec ${scriptGestionUsers} --get-name ${listIdUser} | tail -n 1`  # Appel fonction située dans un autre script
+	if [[ "$listIdUser" == "ALL" ]]; then
+		listIdUser=`cut -f2 -d";" ${fileLog} | sort | uniq`
+		userName="ALL"
+	fi
+	tex_finalResults="${dir_tmpReport}/${userName}_${dateStart}_${dateEnd}.tex"
+	file_finalResults="${userName}_${dateStart}_${dateEnd}"
+	if [ -f ${dir_report}/${tex_finalResults}.pdf ]; then     # très improbable mais bon  :D
+		echo "Le fichier existe déjà."
+		break
+	fi
+	
+	######################################################
+	# Parcours list users :                              #
+	# 1 loop if a user defined has been selected,        #
+	# several loops if the value "ALL" has been selected #
+	######################################################
+	# Using tex_multipleUsers file #
+	################################
+	i=0
+	for currentIdUser in ${listIdUser[*]}
+	do
+		echo -e "listIdUser[${i}] : ${currentIdUser}"
+		#########################
+		# Filtrage selon l'user #
+		#########################
+		local currentUserName=`exec ${scriptGestionUsers} --get-name ${currentIdUser} | tail -n 1`  # Appel fonction située dans un autre script
+		echo -e "1/ User is : $currentUserName (id: $currentIdUser)"
+		tmp_fileCurrentUser=${dir_tmpReport}/${currentIdUser}.tmp
+		cat ${tex_modelOneUser} >> ${tex_multipleUsers}
+		sed -i -e "s/__CURRENT_USER__/${currentUserName} (id: ${currentIdUser})/" ${tex_multipleUsers}
+		grep ";${currentIdUser};${currentUserName};" ${fileLog} > ${tmp_fileCurrentUser}
+		
+		###################################
+		# Filtrage selon la date de début #
+		# et la date de fin               #
+		###################################
+		# TODO : 
+		
+		###################
+		# Graph. protocol #
+		###################
+		#listProtocol=`cut -f7 -d";" ${tmp_fileCurrentUser} | cut -f1 -d":" | sort | uniq`
+		
+		########################
+		# Tab. Repatriation OK #
+		########################
+		echo "Liste de rapatriements réussis"
+		listRepatrOK=`cat ${tmp_fileCurrentUser} | cut -d ";" -f 8 | grep "0:Téléchargement réussie" | sort | uniq -c`
+		codeTex_RapatriementsReussis=""
+		#TODO:
+		#for repatr in ${listRepatrOK[*]};	do
+			codeTex_RapatriementsReussis=`echo "toto \& tata \& tutu \& titi " `
+			echo $codeTex_RapatriementsReussis
+		#done
+		sed -i -e "s/__LIST_REPATR_OK__/${codeTex_RapatriementsReussis}/" ${tex_multipleUsers}
+		
+		########################
+		# Tab. Repatriation KO #
+		########################
+		echo "Liste de rapatriements échoués"
+		listRepatrKO=`cat ${tmp_fileCurrentUser} | cut -d ";" -f 8 | grep "0:Fichier distant pas plus récent que le fichier local" | sort | uniq -c`
+		codeTex_RapatriementsEchoues=""
+		#TODO:
+		#for repatr in ${listRepatrKO[*]};	do
+			codeTex_RapatriementsEchoues=`echo "toto \& tata \& tutu " `
+			echo $ codeTex_RapatriementsEchoues
+		#done
+		sed -i -e "s/__LIST_REPATR_KO__/${codeTex_RapatriementsEchoues}/" ${tex_multipleUsers}
+		
+		chmod 777 ${tex_multipleUsers}
+
+		i=$((i+1))
+	done
+	echo "$i idUser(s) distinct(s) in log file (${fileLog})"
+	
+	########################
+	# Using tex_modelStart #
+	########################
+	echo "tex_finalResults = $tex_finalResults"
+	cat ${tex_modelStart} >> ${tex_finalResults}
+	chmod 777 ${tex_finalResults}
+	
+	####################################
+	# Using tex_modelSummaryForALLuser #
+	####################################
+	if [[ "$userName" == "ALL" ]]; then
+		cat ${tex_modelSummaryForALLuser} >> ${tex_finalResults}
+		chmod 777 ${tex_finalResults}
+		#TODO: sed ....
+	fi
+	
+	###########################
+	# Using tex_modelEnd file #
+	###########################
+	cat ${tex_multipleUsers} >> ${tex_finalResults}
+	chmod 777 ${tex_finalResults}
+	cat ${tex_modelEnd} >> ${tex_finalResults}
+	chmod 777 ${tex_finalResults}
+	
+	#############################
+	# Generation du fichier PDF #
+	#############################
+	pdflatex ${tex_finalResults}
+	
+	####################
+	# Delete tmp files #
+	####################
+	mv ${file_finalResults}.pdf ${dir_report}
+	mv ${tex_finalResults} ${dir_report}
+	rm -rf ${dir_tmpReport}
+	rm -f ${file_finalResults}.log ${file_finalResults}.aux
 }
 
 # Perform the repatriation
@@ -164,6 +312,7 @@ function getData {
 
 	if [[ "$protocolRepatriation" == "ftp" ]]
 	then
+		# TODO: use rsync
 		echo "check ftp : OK"
 		wget --timestamping \
 			--progress=bar \
@@ -178,7 +327,7 @@ function getData {
 		status=$?
 		if [[ "$isToLog" == "yes" ]]
 		then
-			setLogFromWget ${idUser} ${lastNameUser} ${firstNameUser} ${mailUser} ${destRepatriation} ${sourceRepat} ${status}
+			setLogFromWget ${idUser} ${lastNameUser} ${firstNameUser} ${mailUser} ${destRepatriation} ${sourceRepat} ${status} ${remoteFilesRepatriation}
 		fi
 	elif [[ "$protocolRepatriation" == "http" ]]
 	then
@@ -207,13 +356,6 @@ function getData {
 	then
 		rm ${logFileTmp}
 	fi
-
-	#	isRecent= checkFileIsRecent()
-	#
-	#	if (! isRecent)
-	#	{
-	#		wget ftp...fichier.fq
-	#	}
 }
 
 # Check if file to download is not already present in local repertory.
@@ -234,6 +376,8 @@ function checkLastFile {
 #  - Protocol errors
 #  - Server issued an error response (Répertoire ou Fichier source inexistant)
 #  - Others errors
+# Format of one log :
+# $date;$idUser;$lastNameUser;$firstNameUser;$mailUser;$destRepatriation;$sourceRepatriation;$idStatus:$status:$taille
 function setLogFromWget {
 	echo "setLog function called"
 	local idUser=$1
@@ -322,8 +466,8 @@ function setLogFromWget {
 		echo "Error : log file does not exist"
 	fi
 
-	date=`date "+%d/%m/%Y %H:%M:%S"`
-	echo "$date;$lastNameUser;$firstNameUser;$mailUser;$destRepatriation;$sourceRepatriation;$RES" >> ${fileLog}
+	date=`date "+%d-%m-%Y %H:%M:%S"`
+	echo "$date;$idUser;$lastNameUser;$firstNameUser;$mailUser;$destRepatriation;$sourceRepatriation;$RES" >> ${fileLog}
 }
 
 
